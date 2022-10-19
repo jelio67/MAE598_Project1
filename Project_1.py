86fe5317372e600f70fd5f92c51c12587b17b895
jelio67 <jelio@asu.edu>
2022-10-18 21:58:57 -0700

    Working model for controlling angular position along with distance to ground. Tested different neural network parameters but this converged the fastest - in 87 iterations.

--- Project_1.py
+++ Project_1.py
@@ -10,12 +10,13 @@ from torch.nn import utils
 import matplotlib.pyplot as plt
 
 dt = 0.1 # s, time interval
-g = 0.12 # m/s^2? gravitational acceleration
-boost_a = 0.18 # m/s^2? thrust constant
+g = 0.12 # gravitational acceleration
+boost_a = 0.18 # thrust constant
+r_a = 1 #  ratational acceleration
+
 
 w = 0.25 # m, landing platform width
 h = 0.06 # m, landing platform height
-r_a = 20 # rad/s^2? ratational constant
 
 
 class Dynamics(nn.Module):
@@ -26,19 +27,20 @@ class Dynamics(nn.Module):
     @staticmethod
     def forward(state, action):
 
-        # Apply gravity, put this as the second element in a tensor bc
-        delta_state_gravity = g * dt * t.tensor([0., 1.]) # m/s, speed change due to gravitational acceleration
-        # delta_state_gravity = t.tensor([0., g * dt]) # original eqn.
+        # Speed change due to gravitational acceleration
+        delta_v_g = g * dt * t.tensor([0., -1., 0., 0.])
 
-        # Thrust
-        delta_state = boost_a * dt * t.tensor([0., -1.]) * action # original eqn.
+        # Speed change due to thrust
+        delta_v_a = boost_a * dt * t.tensor([0., 1., 0., 0.]) * action[0]
 
+        # Angular velocity change due to rotational thrust
+        delta_omega = r_a * dt * t.tensor([0., 0., 0., -1.]) * (2*action[1]-1)
 
-        # New velocity after considering gravity and thrust
-        state = state + delta_state + delta_state_gravity
+        # Apply changes to velocity and angular velocity
+        state = state + delta_v_a + delta_v_g + delta_omega
 
         # Update state
-        step_mat = t.tensor([[1., dt], [0., 1.]])
+        step_mat = t.tensor([[1., dt, 0., 0.], [0., 1., 0., 0.], [0., 0., 1., dt], [0., 0., 0., 1.]])
         state = t.matmul(step_mat, state)
         # print('State:', state, '\n Action:', action)
 
@@ -53,12 +55,14 @@ class Controller(nn.Module):
         dim_output: # of actions
         dim_hidden: up to you
         """
+        dim_hidden2 = 4
         super(Controller, self).__init__()
         self.network = nn.Sequential(
             nn.Linear(dim_input, dim_hidden),
             nn.Tanh(),
+            # nn.Linear(dim_hidden, dim_hidden2),
+            # nn.Mish(),
             nn.Linear(dim_hidden, dim_output),
-            # You can add more layers here
             nn.Sigmoid()
         )
 
@@ -86,65 +90,86 @@ class Simulation(nn.Module):
             action = self.controller.forward(state)
             state = self.dynamics.forward(state, action)
             self.action_trajectory.append(action)
+            if _ == 80:
+                pass
             self.state_trajectory.append(state)
         return self.error(state)
 
     @staticmethod
     def initialize_state():
-        # x0 = float(np.random.rand(1))*5
+        # y0 = float(np.random.rand(1))*5
         # v0 = float(np.random.rand(1))
+        # theta0 = float(np.random.rand(1))
+        # omega0 = float(np.random.rand(1))
 
-        state = [1., 0.]  # TODO: need batch of initial states
+        # state = [y0, v0, theta0, omega0]
+        state = [1., 0., 0.5, 0.]  # TODO: need batch of initial states
         return t.tensor(state, requires_grad=False).float()
 
     def error(self, state):
-        return state[0]**2 + state[1]**2
+        return state[0]**2 + state[1]**2 + state[2]**2 + state[3]**2
 
 
 
 class Optimize:
     def __init__(self, simulation):
         self.simulation = simulation
-        self.parameters = simulation.controller.parameters()
+        self.parameters = simulation.controller.parameters() # can find gradient from self.parameters.grad?
         self.optimizer = optim.LBFGS(self.parameters, lr=0.01)
 
     def step(self):
         def closure():
             loss = self.simulation(self.simulation.state)
             self.optimizer.zero_grad()
-            loss.backward()
+            loss.backward() # gradient descent step
             return loss
 
         self.optimizer.step(closure)
         return closure()
 
-    def train(self, epochs):
-        for epoch in range(epochs):
+    def train(self, epochs, eps):
+        # for epoch in range(epochs):
+        loss = 1
+        epoch = 0
+        while loss >= eps :
+            epoch += 1
             loss = self.step()
-            print('[%d] loss: %.3f' % (epoch + 1, loss))
-            self.visualize()
+            print('[%d] loss: %.6f' % (epoch + 1, loss))
+            if epoch % 5 == 0:
+                self.visualize(epoch)
 
-    def visualize(self):
+    def visualize(self, i):
         data = np.array([self.simulation.state_trajectory[i].detach().numpy() for i in range(self.simulation.T)])
-        x = data[:, 0] # position
-        y = data[:, 1] # velocity
-        plt.plot(x, y)
+        y = data[:, 0] # position
+        v = data[:, 1] # velocity
+        theta = data[:, 2] # angular position
+        omega = data[:, 3] # angular velocity
+
+        plt.figure()
+        plt.subplot(211)
+        plt.plot(y, v)
         plt.xlabel('Position, d(t)')
         plt.ylabel('Velocity, v(t)')
+        plt.title('iteration:'+str(i))
+        plt.subplot(212)
+        plt.plot(theta, omega)
+        plt.xlabel('Angular Position, \u03B8(t)') # printing greek letters: https://pythonforundergradengineers.com/unicode-characters-in-python.html
+        plt.ylabel('Angular Velocity, \u03C9(t)')
+        plt.subplots_adjust(hspace=0.3)
         plt.show()
 
 
 
-
+eps = 1e-6
 T = 100  # number of time steps
-dim_input = 2  # state space dimensions
-dim_hidden = 6  # latent dimensions
-dim_output = 1  # action space dimensions
+dim_input = 4  # state space dimensions
+dim_hidden = 3  # latent dimensions
+dim_output = 2  # action space dimensions
 d = Dynamics()  # define dynamics
 c = Controller(dim_input, dim_hidden, dim_output)  # define controller
 s = Simulation(c, d, T)  # define simulation
 o = Optimize(s)  # define optimizer
-o.train(40)  # solve the optimization problem
+o.train(40, eps)  # solve the optimization problem, take 40 gradient descent steps, could set another termination criteria
 
 
 
